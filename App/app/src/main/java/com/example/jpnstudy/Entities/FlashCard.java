f0dc903c37ba3af5e313ebc97b118b66e47aaffd
delacruzkb <delacruzkb@g.cofc.edu>
2020-02-23 13:42:16 -0500

    implemented flash card class

--- App/app/src/main/java/com/example/jpnstudy/Entities/FlashCard.java
+++ App/app/src/main/java/com/example/jpnstudy/Entities/FlashCard.java
@@ -1,5 +1,38 @@
 package com.example.jpnstudy.Entities;
 
 public class FlashCard {
-    // TODO: implement flashcard class, make it abstract or interface
+    boolean isStarred,isKnown,isMastered;
+    int rightCounter;
+
+    public boolean isStarred() {
+        return isStarred;
+    }
+
+    public void setStarred(boolean starred) {
+        isStarred = starred;
+    }
+
+    public boolean isKnown() {
+        return isKnown;
+    }
+
+    public void setKnown(boolean known) {
+        isKnown = known;
+    }
+
+    public boolean isMastered() {
+        return isMastered;
+    }
+
+    public void setMastered(boolean mastered) {
+        isMastered = mastered;
+    }
+
+    public int getRightCounter() {
+        return rightCounter;
+    }
+
+    public void setRightCounter(int rightCounter) {
+        this.rightCounter = rightCounter;
+    }
 }
