d1155298ad333a20abaf76216c667674ce2baf7a
delacruzkb <delacruzkb@g.cofc.edu>
2020-02-23 13:43:54 -0500

    added basic implementation of word and grammar

--- App/app/src/main/java/com/example/jpnstudy/Entities/Grammar.java
+++ App/app/src/main/java/com/example/jpnstudy/Entities/Grammar.java
@@ -2,4 +2,30 @@ package com.example.jpnstudy.Entities;
 
 public class Grammar {
     //TODO: implement grammar
+
+    String name,definition,example;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+
+    public String getExample() {
+        return example;
+    }
+
+    public void setExample(String example) {
+        this.example = example;
+    }
 }
--- App/app/src/main/java/com/example/jpnstudy/Entities/Word.java
+++ App/app/src/main/java/com/example/jpnstudy/Entities/Word.java
@@ -2,4 +2,38 @@ package com.example.jpnstudy.Entities;
 
 public class Word {
     //TODO: implement word
+
+    String japanese,english,kanji,type;
+
+    public String getJapanese() {
+        return japanese;
+    }
+
+    public void setJapanese(String japanese) {
+        this.japanese = japanese;
+    }
+
+    public String getEnglish() {
+        return english;
+    }
+
+    public void setEnglish(String english) {
+        this.english = english;
+    }
+
+    public String getKanji() {
+        return kanji;
+    }
+
+    public void setKanji(String kanji) {
+        this.kanji = kanji;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
