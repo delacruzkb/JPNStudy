1cbb425cd164cc2503cac3ccb6a67bfce682a47d
Kenneth Dela Cruz <31312591+delacruzkb@users.noreply.github.com>
2020-02-25 10:32:34 -0500

    Update searchGrammarExample.java

--- App/app/src/main/java/com/example/jpnstudy/AsyncTasks/Search/searchGrammarExample.java
+++ App/app/src/main/java/com/example/jpnstudy/AsyncTasks/Search/searchGrammarExample.java
@@ -1,4 +1,23 @@
 package com.example.jpnstudy.AsyncTasks.Search;
 
-public class searchGrammarExample {
+
+import android.os.AsyncTask;
+
+import com.example.jpnstudy.Database.FlashCardDatabase;
+import com.example.jpnstudy.Entities.Grammar;
+
+
+public class searchGrammarExample extends AsyncTask<Void,Void, ArrayList<Grammar>> {
+    private FlashCardDatabase fcdb;
+    private String searchKey;
+    public SearchGrammarExample(FlashCardDatabase db, String search)
+    {
+        fcdb=db;
+        searchKey = search;
+    }
+
+    @Override
+    protected ArrayList<Grammar> doInBackground(Void... voids) {
+        return (ArrayList)fcdb.grammarDao().searchGrammarExample(searchKey);
+    }
 }
