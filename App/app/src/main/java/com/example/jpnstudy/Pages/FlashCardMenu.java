911b4e05f4e69893b1dbaf8250eae917699b78a8
delacruzkb <delacruzkb@g.cofc.edu>
2020-03-09 19:36:42 -0400

    edited flash card menu, begun ordeal setup: separate layouts for ordeals

--- App/app/src/main/java/com/example/jpnstudy/Pages/FlashCardMenu.java
+++ App/app/src/main/java/com/example/jpnstudy/Pages/FlashCardMenu.java
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.jpnstudy.R;
@@ -15,15 +19,22 @@ public class FlashCardMenu extends AppCompatActivity {
     String mode;
     String cardFront;
     String cardBack;
+    Spinner ordealSpinner;
+    TextView ordealDescription;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flash_card_menu);
         Intent intent = getIntent();
         mode = intent.getStringExtra("mode");
-        if(mode.equals(getString(R.string.ordeal_label)))
+        if(!mode.equals(getString(R.string.ordeal_label)))
         {
-            menuForOrdealMode();
+            LinearLayout ordealLayout = findViewById(R.id.flash_card_menu_ordeal_layout);
+            ordealLayout.setVisibility(View.GONE);
+        }
+        else
+        {
+            ordealModeSetup();
         }
     }
     public void submit(View view){
@@ -40,15 +51,15 @@ public class FlashCardMenu extends AppCompatActivity {
             Toast.makeText(this,getText(R.string.same_card_error), Toast.LENGTH_SHORT).show();
         }
         else{
-            
-            //TODO: handle different modes
             int amount = Integer.parseInt(amountEdit.getText().toString());
             Intent intent = new Intent(this, FlashCardPage.class);
+            if(mode.equals(getString(R.string.ordeal_label))){
+               mode= ordealSpinner.getSelectedItem().toString();
+            }
             intent.putExtra("cardFront",cardFront);
             intent.putExtra("cardBack",cardBack);
-            intent.putExtra("amount",amount);
+            intent.putExtra("amount", amount);
             intent.putExtra("mode",mode);
-
             startActivity(intent);
         }
     }
@@ -71,9 +82,42 @@ public class FlashCardMenu extends AppCompatActivity {
         }
     }
 
-        
-    private void menuForOrdealMode(){
-        //setup extra view?
+    private void ordealModeSetup() {
+        ordealDescription = findViewById(R.id.flash_card_menu_ordeal_description_text_view);
+        ordealDescription.setText(getString(R.string.flash_card_menu_ordeal_multiple_choice_description));
+        ordealSpinner= findViewById(R.id.flash_card_menu_ordeal_spinner);
+        ordealSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                switch (position)
+                {
+                    case 0:
+                        ordealDescription.setText(getString(R.string.flash_card_menu_ordeal_multiple_choice_description));
+                        break;
+                    case 1:
+                        ordealDescription.setText(getString(R.string.flash_card_menu_ordeal_character_choice_description));
+                        break;
+                    case 2:
+                        ordealDescription.setText(getString(R.string.flash_card_menu_ordeal_key_in_description));
+                        break;
+                    case 3:
+                        ordealDescription.setText(getString(R.string.flash_card_menu_ordeal_mixed_description));
+                        break;
+                    case 4:
+                        ordealDescription.setText(getString(R.string.flash_card_menu_ordeal_weighted_description));
+                        break;
+                    default:
+                        ordealDescription.setText(getString(R.string.flash_card_menu_ordeal_multiple_choice_description));
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
     }
-    
 }
+
